ad12cea6f0667188f84b3a310cfcb2e4a8f6fe27
arva karthik <arvakarthik01@gmail.com>
2026-08-28 22:28:15 +0530

    Remove unnecessary backup folder

--- cyberaudit-backup/utils/privileges.py
+++
@@ -1,7 +0,0 @@
-def is_admin():
-    """Return True if running with admin privileges (Windows)."""
-    try:
-        import ctypes
-        return ctypes.windll.shell32.IsUserAnAdmin() != 0
-    except Exception:
-        return False
